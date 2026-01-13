c916f39b3c371a4a834f3b65e87b649fc0eb48dd
MOHAMED ANWAARULLAH M <128156082@sastra.ac.in>
2026-01-14 00:11:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,32 +3,34 @@ import asyncio
 from telegram import Update
 from telegram.ext import Application, ChatMemberHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEn")
+print("BOT STARTING...")
 
-# Customize these
-WELCOME_TEXT = "Welcome {username} 👋\nPlease read the rules and enjoy the channel!"
-DELETE_AFTER = 10  # seconds
+BOT_TOKEN = os.getenv("BOT_TOKEn")
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN is missing")
+
+WELCOME_TEXT = "Welcome {username} 👋"
+DELETE_AFTER = 10
 
 async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
     result = update.chat_member
-
     if result.new_chat_member.status == "member":
         user = result.new_chat_member.user
         username = f"@{user.username}" if user.username else user.first_name
 
-        message = await context.bot.send_message(
+        msg = await context.bot.send_message(
             chat_id=update.chat_member.chat.id,
             text=WELCOME_TEXT.format(username=username)
         )
 
         await asyncio.sleep(DELETE_AFTER)
-        await message.delete()
+        await msg.delete()
 
 def main():
+    print("BOT RUNNING...")
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(ChatMemberHandler(welcome, ChatMemberHandler.CHAT_MEMBER))
     app.run_polling()
 
 if __name__ == "__main__":
     main()
-
